1b724ef875c79487f97d47ff56b20abdcdbdeb9a
nwhye <hannaasiadouskaya@gmail.com>
2025-10-25 13:31:46 +0300

    upload

---
+++ 1st semester/Procedural Programming/LW 4.1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	int s = 0;
+	int sum;
+	cout << "Enter your number: ";
+	cin >> a;
+	sum = a*(a + 1) / 2;
+	cout << "the sum of natural numbers ie equal " << sum;
+}
