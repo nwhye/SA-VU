fc301a7ffe9a727098d23718fc0390ed5855129f
nwhye <hannaasiadouskaya@gmail.com>
2025-11-11 12:51:54 +0200

    upload

---
+++ 2nd semester/Data Structure/LW 8.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D'],
+    'C': ['A', 'D', 'E'],
+    'D': ['B', 'C', 'E'],
+    'E': ['C', 'D']
+}
+
+
+def bfs(graph, start):
+    visited = set()  
+    queue = deque([start])
+    visited.add(start)
+
+    while queue:
+        vertex = queue.popleft()
+        print(vertex)
+
+        for neighbor in graph[vertex]:
+            if neighbor not in visited:
+                queue.append(neighbor)
+                visited.add(neighbor)
+
+
+bfs(graph, 'A')
